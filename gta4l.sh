c289698aa67a75b1115a9a2d036cea2f46a6b5f9
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-06-13 07:30:49 +0530

    Create gta4l.sh

---
+++ gta4l.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# Clean old manifest
+rm -rf .repo/local_manifests
+
+# Init Infinity Source
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+
+# Add local manifest
+mkdir -p .repo/local_manifests
+
+# Download manifest
+git clone https://github.com/shipukacapri/gta4l_local_manifest --depth 1 -b inf-qp2 .repo/local_manifests
+
+# Clean previous sources
+rm -rf device/samsung/gta4l
+rm -rf device/samsung/gta4l-common
+rm -rf kernel/samsung/sm6115
+rm -rf vendor/samsung/gta4l
+rm -rf vendor/samsung/gta4l-common
+rm -rf hardware/samsung
+rm -rf hardware/dolby
+
+# Sync everything from manifest
+/opt/crave/resync.sh
+
+# Clean previous builds
+rm -rf out/target/product/capri
+
+# Optional conflict fixes
+
+# Build environment
+export BUILD_USERNAME=Shipu
+export BUILD_HOSTNAME=Crave
+export TZ="Asia/Kolkata"
+export SKIP_ABI_CHECKS=true
+
+# Build
+source build/envsetup.sh
+lunch infinity_gta4l-userdebug
+
+m bacon
