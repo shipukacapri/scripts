56ef582d23e1faef909a8d1f191b137fa9b24599
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-06-26 20:32:23 +0530

    Update manaus.sh

--- manaus.sh
+++ manaus.sh
@@ -8,11 +8,11 @@ rm -rf .repo/local_manifests
 # Init Lineage Source
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 
-# Add local manifest
-mkdir -p .repo/local_manifests
+# local manifest
+git clone https://github.com/shipukacapri/manaus_local_manifests -b lineage .repo/local_manifests --depth=1
 
-# Download manifest
-git clone https://github.com/shipukacapri/manaus_local_manifests --depth 1 -b lineage .repo/local_manifests
+# Sync everything from manifest
+/opt/crave/resync.sh
 
 # Clean previous sources
 rm -rf device/motorola/manaus
@@ -22,11 +22,13 @@ rm -rf kernel/motorola/manaus
 rm -rf hardware/motorola
 rm -rf device/mediatek/sepolicy_vndr
 
-# Sync everything from manifest
-/opt/crave/resync.sh
-
-# Clean previous builds
-rm -rf out/target/product/manaus
+# Clone Device Trees
+git clone https://github.com/shipukacapri/device_motorola_manaus.git -b lineage device/motorola/manaus --depth=1
+git clone https://github.com/moto-manaus/device_motorola_manaus-kernel.git -b sixteen device/motorola/manaus-kernel --depth=1
+git clone https://codeberg.org/rexix01/vendor_motorola_manaus.git -b lineage-23.2 vendor/motorola/manaus --depth=1
+git clone https://github.com/moto-manaus/device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr --depth=1
+git clone https://github.com/moto-manaus/hardware_mediatek.git -b lineage-23.2 hardware/mediatek --depth=1
+git clone https://github.com/shipsingh2002/hardware_motorola.git -b edited hardware/motorola --depth=1
 
 # Optional conflict fixes
 
